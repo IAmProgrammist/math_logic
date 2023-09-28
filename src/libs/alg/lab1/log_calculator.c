825474b4726412767c30b95b43e63c57b01b62d6
vladi <vladislav.pakhomov@bk.ru>
2023-09-28 17:14:49 +0300

    WIP

--- src/libs/alg/lab1/log_calculator.c
+++ src/libs/alg/lab1/log_calculator.c
@@ -50,6 +50,21 @@ void processFormula(Formula *formula, char *line)
     if (type == INPUT_TYPE_INVALID)
         return;
 
+    if (type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
+    {
+        return _processFormulaDisjunctive(formula, line);
+    }
+
+    if (type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM)
+    {
+        return _processFormulaConjunctive(formula, line);
+    }
+}
+
+static void _processFormulaDisjunctive(Formula *formula, char *line)
+{
+    // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
+
     int bracesCount = 0;
     int presence = LITERAL_UNDEF;
     int letterIndex = -1;
@@ -61,29 +76,6 @@ void processFormula(Formula *formula, char *line)
         if (isspace(input))
         {
             // Ignore
-        }
-        else if (input == OPENING_BRACKET)
-        {
-            if (presence != LITERAL_UNDEF || letterIndex != -1 || type != INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM || bracesCount != 0)
-            {
-                fprintf(stderr, "Parsing error at index %d", index);
-                return;
-            }
-
-            bracesCount++;
-        }
-        else if (input == CLOSING_BRACKET)
-        {
-            if (letterIndex == -1 || type != INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM || bracesCount != 1)
-            {
-                fprintf(stderr, "Parsing error at index %d", index);
-                return;
-            }
-
-            element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
-            presence = LITERAL_UNDEF;
-            letterIndex = -1;
-            bracesCount--;
         }
         else if (input == NON)
         {
@@ -98,74 +90,37 @@ void processFormula(Formula *formula, char *line)
         }
         else if (input == CONJUNCTION)
         {
-            // &
-            if (type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM)
+            if (letterIndex == -1)
             {
-                // (... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)
-                if (bracesCount == 0)
-                {
-                    addElement(formula, element);
-                    memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
-                }
-                else
-                {
-                    fprintf(stderr, "Brackets error at index %d", index);
-                    return;
-                }
+                fprintf(stderr, "No literal present at index %d", index);
+                return;
             }
-            else
-            {
-                // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
-                if (letterIndex == -1)
-                {
-                    fprintf(stderr, "No literal present at index %d", index);
-                    return;
-                }
 
-                element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
-                presence = LITERAL_UNDEF;
-                letterIndex = -1;
-            }
+            element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
+            presence = LITERAL_UNDEF;
+            letterIndex = -1;
         }
         else if (input == DISJUNCTION)
         {
-            // +
-            if (type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
+            if (letterIndex == -1)
             {
-                // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
-                if (letterIndex == -1)
-                {
-                    fprintf(stderr, "No literal present at index %d", index);
-                    return;
-                }
+                fprintf(stderr, "No literal present at index %d", index);
+                return;
+            }
 
-                element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
-                presence = LITERAL_UNDEF;
-                letterIndex = -1;
+            element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
+            presence = LITERAL_UNDEF;
+            letterIndex = -1;
 
-                if (bracesCount == 0)
-                {
-                    addElement(formula, element);
-                    memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
-                }
-                else
-                {
-                    fprintf(stderr, "Brackets error at index %d", index);
-                    return;
-                }
+            if (bracesCount == 0)
+            {
+                addElement(formula, element);
+                memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
             }
             else
             {
-                // (... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)
-                if (letterIndex == -1)
-                {
-                    fprintf(stderr, "No literal present at index %d", index);
-                    return;
-                }
-
-                element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
-                presence = LITERAL_UNDEF;
-                letterIndex = -1;
+                fprintf(stderr, "Brackets error at index %d", index);
+                return;
             }
         }
         else if (isupper(input) || islower(input))
@@ -174,17 +129,14 @@ void processFormula(Formula *formula, char *line)
         }
         else if (input == '\0')
         {
-            if (type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
+            // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
+            if (letterIndex == -1)
             {
-                // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
-                if (letterIndex == -1)
-                {
-                    fprintf(stderr, "No literal present at index %d", index);
-                    return;
-                }
-
-                element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
+                fprintf(stderr, "No literal present at index %d", index);
+                return;
             }
+
+            element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
 
             if (bracesCount == 0)
             {
@@ -209,11 +161,183 @@ void processFormula(Formula *formula, char *line)
     }
 }
 
-bool findVal(Formula f, bool val[LATIN_ALPHABET_LENGTH])
+static void _processFormulaConjunctive(Formula *formula, char *line)
 {
-    int type = checkInputType(f.type);
+    // (... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)
+
+    int bracesCount = 0;
+    int presence = LITERAL_UNDEF;
+    int letterIndex = -1;
+    char element[LATIN_ALPHABET_LENGTH] = {};
+    int index = 0;
+    while (1)
+    {
+        char input = *line;
+        if (isspace(input))
+        {
+            // Ignore
+        }
+        else if (input == OPENING_BRACKET)
+        {
+            if (presence != LITERAL_UNDEF || letterIndex != -1 || bracesCount != 0)
+            {
+                fprintf(stderr, "Parsing error at index %d", index);
+                return;
+            }
+
+            bracesCount++;
+        }
+        else if (input == CLOSING_BRACKET)
+        {
+            if (letterIndex == -1 || bracesCount != 1)
+            {
+                fprintf(stderr, "Parsing error at index %d", index);
+                return;
+            }
+
+            element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
+            presence = LITERAL_UNDEF;
+            letterIndex = -1;
+            bracesCount--;
+        }
+        else if (input == NON)
+        {
+            if (letterIndex != -1)
+            {
+                fprintf(stderr, "Parsing error at index %d", index);
+                return;
+            }
+
+            presence = presence == LITERAL_UNDEF ? LITERAL_NEGATIVE : presence == LITERAL_POSITIVE ? LITERAL_NEGATIVE
+                                                                                                   : LITERAL_POSITIVE;
+        }
+        else if (input == CONJUNCTION)
+        {
+            if (bracesCount == 0)
+            {
+                addElement(formula, element);
+                memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
+            }
+            else
+            {
+                fprintf(stderr, "Brackets error at index %d", index);
+                return;
+            }
+        }
+        else if (input == DISJUNCTION)
+        {
+
+            // (... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)
+            if (letterIndex == -1)
+            {
+                fprintf(stderr, "No literal present at index %d", index);
+                return;
+            }
+
+            element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
+            presence = LITERAL_UNDEF;
+            letterIndex = -1;
+        }
+        else if (isupper(input) || islower(input))
+        {
+            letterIndex = charAlphabetIndex(islower(input) ? toupper(input) : input);
+        }
+        else if (input == '\0')
+        {
+            if (bracesCount == 0)
+            {
+                addElement(formula, element);
+                memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
+            }
+            else
+            {
+                fprintf(stderr, "Brackets error at index %d", index);
+                return;
+            }
+
+            break;
+        }
+        else
+        {
+            fprintf(stderr, "Unknown character '%c' at index %d", input, index);
+            return;
+        }
+
+        line++, index++;
+    }
+}
+
+bool _findValConjunctive(Formula f, bool val[LATIN_ALPHABET_LENGTH]) {
+    bool result = 1;
+    for (int i = 0; i < f.amount; i++)
+    {
+        char *element = f.val[i];
+        bool elementResult = 0;
+
+        for (int j = 0; j < LATIN_ALPHABET_LENGTH; j++)
+        {
+            if (element[j] == LITERAL_UNDEF)
+                continue;
+
+            int current = (element[j] == LITERAL_POSITIVE) && val[j];
+
+            if (current)
+            {
+                elementResult = 1;
+                break;
+            }
+        }
+
+        if (!elementResult)
+            return false;
+    }
+
+    return result;
+}
+
+bool _findValDisjunctive(Formula f, bool val[LATIN_ALPHABET_LENGTH]) {
+    for (int i = 0; i < f.amount; i++)
+    {
+        char *element = f.val[i];
+        bool elementResult = true;
+
+        for (int j = 0; j < LATIN_ALPHABET_LENGTH; j++)
+        {
+            if (element[j] == LITERAL_UNDEF)
+                continue;
+
+            int current = (element[j] == LITERAL_POSITIVE) && val[j];
+            if (!current)
+            {
+                elementResult = false;
+                break;
+            }
+        }
+
+        if (elementResult)
+            return true;
+    }
+
+    return false;
+}
+
+bool findVal(Formula formula, bool args[LATIN_ALPHABET_LENGTH])
+{
+    int type = checkInputType(formula.type);
     if (type == INPUT_TYPE_INVALID)
-        return;
+        return false;
+
+    if (type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
+    {
+        return _findValDisjunctive(formula, args);
+    }
+
+    if (type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM)
+    {
+        return _findValConjunctive(formula, args);
+    }
+
+    return false;
 
     bool result = type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM ? 0 : 1;
     for (int i = 0; i < f.amount; i++)
@@ -239,8 +363,10 @@ bool findVal(Formula f, bool val[LATIN_ALPHABET_LENGTH])
             }
         }
 
-        if (elementResult && type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM) return 1;
-        if (!elementResult && type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM) return 0;
+        if (elementResult && type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
+            return 1;
+        if (!elementResult && type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM)
+            return 0;
     }
 
     return result;
