eee66d7d274dcc26df2fe3b8c3b9ea6933b6596e
IAmProgrammist <vladislav.pakhomov@bk.ru>
2023-10-03 21:34:27 +0300

    Comments added

--- src/libs/alg/lab1/log_calculator.c
+++ src/libs/alg/lab1/log_calculator.c
@@ -8,39 +8,47 @@
 
 int checkInputType(int type)
 {
-    if (type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM)
-        return INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM;
-    if (type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
-        return INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM;
+    // Если тип соответствует существующим возможным типам, возращаем этот тип. 
+    if (type == INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM || type == INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM)
+        return type;
+
+    // Иначе - невалидный тип
     return INPUT_TYPE_INVALID;
 }
 
 Formula initFormula(int type)
 {
+    // Если тип невалидный - возвращаем формулу с ошибочным типом
     if (checkInputType(type) == INPUT_TYPE_INVALID)
-        return (Formula){NULL, -1, -1, INPUT_TYPE_INVALID};
+        return (Formula){NULL, 0, 0, INPUT_TYPE_INVALID};
     return (Formula){malloc(0), 0, 0, type};
 }
 
 void freeFormula(Formula *formula)
 {
+    // Удаляем каждый элемент формулы
     for (int i = 0; i < formula->amount; i++)
         free(formula->val[i]);
 
+    // Сам массив формул
     free(formula->val);
 
+    // Делаем формулу "невалидной"
     formula->val = NULL;
-    formula->amount = -1;
-    formula->capacity = -1;
+    formula->amount = 0;
+    formula->capacity = 0;
     formula->type = INPUT_TYPE_INVALID;
 }
 
 void addElement(Formula *formula, char element[LATIN_ALPHABET_LENGTH])
 {
+    // Если количество больше вместимости, увеличиваем размер массива элементов в 2 раза + 1
     if (formula->amount >= formula->capacity)
         formula->val = realloc(formula->val, formula->capacity = (formula->capacity * 2 + 1));
 
+    // Инициализируем массив переменных элемента формулы
     formula->val[formula->amount++] = malloc(sizeof(char) * LATIN_ALPHABET_LENGTH);
+    // Копируем переменные в элемент формулы
     memcpy(formula->val[formula->amount - 1], element, sizeof(char) * LATIN_ALPHABET_LENGTH);
 }
 
@@ -48,7 +56,6 @@ static void _processFormulaDisjunctive(Formula *formula, char *line)
 {
     // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
 
-    int bracesCount = 0;
     int presence = LITERAL_UNDEF;
     int letterIndex = -1;
     char element[LATIN_ALPHABET_LENGTH] = {};
@@ -58,61 +65,63 @@ static void _processFormulaDisjunctive(Formula *formula, char *line)
         char input = *line;
         if (isspace(input))
         {
-            // Ignore
+            // Игнорируем, таким образом пробелы, табуляции и т.д. не будут иметь значения.
         }
         else if (input == NON)
         {
+            // Отрицание должно стоять до переменной, а не после.
             if (letterIndex != -1)
             {
                 fprintf(stderr, "Parsing error at index %d", index);
                 return;
             }
-
+            
+            // Если отрицаний не было, значит переменная стоит с знаком !.
+            // Если отрицание было, то !! = отсутствие отрицаний.
             presence = presence == LITERAL_UNDEF ? LITERAL_NEGATIVE : presence == LITERAL_POSITIVE ? LITERAL_NEGATIVE
                                                                                                    : LITERAL_POSITIVE;
         }
         else if (input == CONJUNCTION)
         {
+            // Исключение ситуации, подобной "&&".
             if (letterIndex == -1)
             {
                 fprintf(stderr, "No literal present at index %d", index);
                 return;
             }
 
+            // Если встречаем &, сохраняем предыдущую переменную в элемент
             element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
             presence = LITERAL_UNDEF;
             letterIndex = -1;
         }
         else if (input == DISJUNCTION)
         {
+            // Исключение ситуации, подобной "++".
             if (letterIndex == -1)
             {
                 fprintf(stderr, "No literal present at index %d", index);
                 return;
             }
 
+            // Если встречаем +, сохраняем предыдущую переменную в элемент
             element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
             presence = LITERAL_UNDEF;
             letterIndex = -1;
-
-            if (bracesCount == 0)
-            {
-                addElement(formula, element);
-                memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
-            }
-            else
-            {
-                fprintf(stderr, "Brackets error at index %d", index);
-                return;
-            }
+            
+            // А также сам элемент в формулу. Также обновляем буфферный элемент element.
+            addElement(formula, element);
+            memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
+            
         }
         else if (isupper(input) || islower(input))
         {
+            // Найдена буква латинского алфавита. Выполняем дополнииельные проверки и преобразования так, чтобы 'x' == 'X'.
             letterIndex = charAlphabetIndex(islower(input) ? toupper(input) : input);
         }
         else if (input == '\0')
         {
-            // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
+            // Если дошли до конца строки, сохраняем переменную в элемент, элемент в формулу.
             if (letterIndex == -1)
             {
                 fprintf(stderr, "No literal present at index %d", index);
@@ -121,17 +130,10 @@ static void _processFormulaDisjunctive(Formula *formula, char *line)
 
             element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
 
-            if (bracesCount == 0)
-            {
-                addElement(formula, element);
-                memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
-            }
-            else
-            {
-                fprintf(stderr, "Brackets error at index %d", index);
-                return;
-            }
+            addElement(formula, element);
+            memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
 
+            // Заканчиваем цикл чтения.
             break;
         }
         else
@@ -152,17 +154,19 @@ static void _processFormulaConjunctive(Formula *formula, char *line)
     int presence = LITERAL_UNDEF;
     int letterIndex = -1;
     char element[LATIN_ALPHABET_LENGTH] = {};
+    bool elementEmpty = true;
     int index = 0;
     while (1)
     {
         char input = *line;
         if (isspace(input))
         {
-            // Ignore
+            // Игнорируем, таким образом пробелы, табуляции и т.д. не будут иметь значения.
         }
         else if (input == OPENING_BRACKET)
         {
-            if (presence != LITERAL_UNDEF || letterIndex != -1 || bracesCount != 0)
+            // Открывающая скобка возможна, если степень вложенности равна 0, она не содержится внутри конъюнкта, конъюнкт пустой. 
+            if (presence != LITERAL_UNDEF || letterIndex != -1 || bracesCount != 0 || !elementEmpty)
             {
                 fprintf(stderr, "Parsing error at index %d", index);
                 return;
@@ -172,34 +176,42 @@ static void _processFormulaConjunctive(Formula *formula, char *line)
         }
         else if (input == CLOSING_BRACKET)
         {
+            // Открывающая скобка возможна, если степень вложенности равна 1, а также конъюнкт не пустой.
             if (letterIndex == -1 || bracesCount != 1)
             {
                 fprintf(stderr, "Parsing error at index %d", index);
                 return;
             }
 
+            // Сохраняем переменную в элемент.
             element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
             presence = LITERAL_UNDEF;
             letterIndex = -1;
             bracesCount--;
+            elementEmpty = false;
         }
         else if (input == NON)
         {
+            // Отрицание должно стоять до переменной, а не после.
             if (letterIndex != -1)
             {
                 fprintf(stderr, "Parsing error at index %d", index);
                 return;
             }
 
+            // Если отрицаний не было, значит переменная стоит с знаком !.
+            // Если отрицание было, то !! = отсутствие отрицаний.
             presence = presence == LITERAL_UNDEF ? LITERAL_NEGATIVE : presence == LITERAL_POSITIVE ? LITERAL_NEGATIVE
                                                                                                    : LITERAL_POSITIVE;
         }
         else if (input == CONJUNCTION)
         {
+            // Встретили &. Если вложенность равна 0, записываем элемент в формулу.
             if (bracesCount == 0)
             {
                 addElement(formula, element);
                 memset(element, LITERAL_UNDEF, LATIN_ALPHABET_LENGTH);
+                elementEmpty = true;
             }
             else
             {
@@ -209,8 +221,7 @@ static void _processFormulaConjunctive(Formula *formula, char *line)
         }
         else if (input == DISJUNCTION)
         {
-
-            // (... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)
+            // Встретили +. Сохраняем переменную в элемент.
             if (letterIndex == -1)
             {
                 fprintf(stderr, "No literal present at index %d", index);
@@ -220,13 +231,16 @@ static void _processFormulaConjunctive(Formula *formula, char *line)
             element[letterIndex] = presence == LITERAL_UNDEF ? LITERAL_POSITIVE : presence;
             presence = LITERAL_UNDEF;
             letterIndex = -1;
+            elementEmpty = false;
         }
         else if (isupper(input) || islower(input))
         {
+            // Найдена буква латинского алфавита. Выполняем дополнииельные проверки и преобразования так, чтобы 'x' == 'X'.
             letterIndex = charAlphabetIndex(islower(input) ? toupper(input) : input);
         }
         else if (input == '\0')
         {
+            // Дошли до конца строки. Записываем элемент в формулу. Проверяем, что степень вложенности == 0.
             if (bracesCount == 0)
             {
                 addElement(formula, element);
@@ -238,6 +252,7 @@ static void _processFormulaConjunctive(Formula *formula, char *line)
                 return;
             }
 
+            // Заканчиваем цикл чтения.
             break;
         }
         else
@@ -252,6 +267,7 @@ static void _processFormulaConjunctive(Formula *formula, char *line)
 
 void processFormula(Formula *formula, char *line)
 {
+    // В зависимости от типа формулы выполняем парсинг при помощи соответствующей функции.
     int type = checkInputType(formula->type);
     if (type == INPUT_TYPE_INVALID)
         return;
@@ -268,18 +284,25 @@ void processFormula(Formula *formula, char *line)
 }
 
 static bool _findValConjunctive(Formula f, bool val[LATIN_ALPHABET_LENGTH]) {
+    // (... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)
+    // Вычисления выполняются по ленивой схеме.
+    
     for (int i = 0; i < f.amount; i++)
     {
         char *element = f.val[i];
-        bool elementResult = 0;
+        // Предположим, что результат элемента ... + ... + ... - false.
+        bool elementResult = false;
 
         for (int j = 0; j < LATIN_ALPHABET_LENGTH; j++)
         {
+            // Если переменной нет в формуле, пропускаем её.
             if (element[j] == LITERAL_UNDEF)
                 continue;
 
+            // Вычисляем значение элемента.
             int current = (element[j] == LITERAL_POSITIVE) ? val[j] : !val[j];
 
+            // Если значение переменной true - присваиваем всему элементу true и заканчиваем цикл, дальнейшее вычисление не нужно.
             if (current)
             {
                 elementResult = true;
@@ -287,25 +310,33 @@ static bool _findValConjunctive(Formula f, bool val[LATIN_ALPHABET_LENGTH]) {
             }
         }
 
+        // Если элемент формулы false, то формула вида ... & ... & ... тоже будет иметь результат false 
         if (!elementResult)
             return false;
     }
 
+    // Все элементы ... & ... & ... true, формула тоже true.
     return true;
 }
 
 static bool _findValDisjunctive(Formula f, bool val[LATIN_ALPHABET_LENGTH]) {
+    // ... & ... & ... + ... & ... & ... & ... + ... & ... & ...
+    // Вычисления выполняются по ленивой схеме.
     for (int i = 0; i < f.amount; i++)
     {
         char *element = f.val[i];
+        // Предположим, что результат элемента ... & ... & ... - true.
         bool elementResult = true;
 
         for (int j = 0; j < LATIN_ALPHABET_LENGTH; j++)
         {
+            // Если переменной нет в формуле, пропускаем её.
             if (element[j] == LITERAL_UNDEF)
                 continue;
 
+            // Вычисляем значение элемента.
             int current = (element[j] == LITERAL_POSITIVE) ? val[j] : !val[j];
+            // Если значение переменной false - присваиваем всему элементу false и заканчиваем цикл, дальнейшее вычисление не нужно.
             if (!current)
             {
                 elementResult = false;
@@ -313,15 +344,18 @@ static bool _findValDisjunctive(Formula f, bool val[LATIN_ALPHABET_LENGTH]) {
             }
         }
 
+        // Если элемент формулы true, то формула вида ... + ... + ... тоже будет иметь результат true 
         if (elementResult)
             return true;
     }
 
+    // Все элементы оказались false, возвращаем false.
     return false;
 }
 
 bool findVal(Formula formula, bool args[LATIN_ALPHABET_LENGTH])
 {
+    // В зависимости от типа формулы выполняем вычисление значения формулы.
     int type = checkInputType(formula.type);
     if (type == INPUT_TYPE_INVALID)
         return false;
--- src/libs/alg/lab1/log_calculator.h
+++ src/libs/alg/lab1/log_calculator.h
@@ -21,22 +21,36 @@
 #define charAlphabetIndex(val) ((val) - 'A')
 
 typedef struct Formula {
+    // Элементы формулы, конъюнкты или дизъюнкты в зависимости от type
     char **val;
+    // Длина массива элементов
     int amount;
+    // Вместимость массива элементов
     int capacity;
+    // Тип формулы
     int type;
 } Formula;
 
+// Принимает тип формулы type, возвращает INPUT_TYPE_DISJUNCTIVE_NORMAL_FORM, если переданный тип соответствует дизъюнктивной нормальной форме,
+// INPUT_TYPE_CONJUNCTIVE_NORMAL_FORM, если переданный тип соответствует конъюнктивной нормальной форме, иначе - INPUT_TYPE_INVALID.
 int checkInputType(int type);
 
+// Принимает тип формулы type и на его основе инициализирует и возвращает формулу.
+// Если type невалидный, возвращает формулу с типом INPUT_TYPE_INVALID.
 Formula initFormula(int type);
 
+// Освобождает из памяти ресурсы, занятые formula
 void freeFormula(Formula *formula);
 
+// Добавляет element (конъюнкт или дизъюнкт в зависимости от типа формулы) в formula.
 void addElement(Formula *formula, char element[LATIN_ALPHABET_LENGTH]);
 
+// Выполняет парсинг формулы и сохраняет её в formula из строки line.
+// Формула в конъюнктивной нормальной форме должна иметь вид "(... + ... + ...) & (... + ... + ... + ...) & (... + ... + ...)"
+// Формула в дизъюнктивной нормальной форме должна иметь вид "... & ... & ... + ... & ... & ... & ... + ... & ... & ..."
 void processFormula(Formula *formula, char* line);
 
+// Возвращает значение формулы f при значениях переменных из val.
 bool findVal(Formula f, bool val[LATIN_ALPHABET_LENGTH]);
 
 #endif //MATH_LOGIC_LOG_CALCULATOR_H
